f53d9fc4fae03570e843f12e5d2e8ae18b545096
Fleli <frederikedvardsen@gmail.com>
2024-03-11 15:56:51 +0100

    Added missing sudo access

--- src/scripts/lib_pkg.c
+++ src/scripts/lib_pkg.c
@@ -7,7 +7,7 @@ char *install_lib_pkg(char *github, char *category, char *result_folder) {
         "cd /opt/cpm"
         " && sudo mkdir -p libraries/", category,
         " && cd libraries/", category,
-        " && rm -rf ", result_folder,
+        " && sudo rm -rf ", result_folder,
         " && sudo git clone ", github
     );
     
